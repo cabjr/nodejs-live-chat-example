36c67f73eed738b1e5a74f8fba8080850e92ff30
Carlos <cabertoncelli@gmail.com>
2022-10-01 15:58:46 -0300

    Added middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
+import { requireJwtMiddleware } from '../middlewares/authMiddleware'
 
 const routes = Router();
 export default routes
-        .get('/', userController.ListUsers)
+        .get('/', requireJwtMiddleware ,userController.ListUsers)
         .post('/', userController.CreateUser)
         .post('/check', userController.checkAuth)
         .post('/auth', userController.authenticate);
