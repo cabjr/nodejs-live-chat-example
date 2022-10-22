8a1d93323c3db6f0791e078d40b9e43fb4696906
Carlos <cabertoncelli@gmail.com>
2022-10-22 12:43:26 -0300

    Added things inside audiocontroller

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import * as Websocket from 'ws';
 import * as http from 'http';
 import apiRouter from './router';
+import webSocketHandler from './controllers/audioController';
 
 const server = express();
 
@@ -35,21 +36,7 @@ const httpServer = http.createServer(server);
 
 const webSocketServer = new Websocket.Server({server: httpServer, path: '/ws'});
 
-webSocketServer.on('connection', (ws: WebSocket) => {
-
-  //connection is up, let's add a simple simple event
-  ws.on('message', (message: string) => {
-
-      //log the received message and send it back to the client
-      console.log('received: %s', message);
-      ws.send(`Hello, you sent -> ${message}`);
-  });
-
-  //send immediatly a feedback to the incoming connection    
-  ws.send('Hi there, I am a WebSocket server');
-});
-
-
+webSocketServer.on('connection', webSocketHandler);
 
 server.use('/', apiRouter);
 
--- src/controllers/audioController.ts
+++ src/controllers/audioController.ts
@@ -1,2 +1,16 @@
+import * as WebSocket from 'ws';
 
+const webSocketHandler = (ws: WebSocket) => {
+    console.log("New client connected (WebSocket)");
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+  
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Connected to the websocket server!');
+  };
 
+  export default webSocketHandler;
